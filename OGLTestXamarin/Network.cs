ef208c54bc2a4e11042b8649a34e3fb78d405d0b
tertis <tertis@gmail.com>
2016-06-01 23:21:48 +0900

    [Network] Add Mono Test

---
+++ OGLTestXamarin/Network.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace OGLTestXamarin
+{		
+	[TestFixture]
+	public class Network
+	{
+		OGL.Network.TCP.Server server;
+		OGL.Network.TCP.Client client;
+
+		[TestFixtureSetUp]
+		public void Setup()
+		{
+			server = new OGL.Network.TCP.Server();
+			server.Start();
+		}
+			
+		[Test]
+		public void StartClient()
+		{
+			client = new OGL.Network.TCP.Client();
+			client.Start("127.0.0.1");
+		}
+
+		[TestFixtureTearDown]
+		public void CleanUp()
+		{
+			server.Stop();
+			Trace.Write("Test End");
+		}
+	}
+}
