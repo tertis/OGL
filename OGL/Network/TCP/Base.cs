036ebc4518d09861c843b21c5904ced091abaae5
tertis <tertis@gmail.com>
2016-06-05 22:19:12 +0900

    [Network] Add Base file

---
+++ OGL/Network/TCP/Base.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace OGL.Network.TCP
+{
+	public abstract class Base
+	{
+		#region Send
+		protected void SendToRemote(Socket socket, byte[] data)
+		{
+			// Begin sending the data to the remote device.
+			socket.BeginSend(data, 0, data.Length, 0,
+				new AsyncCallback(SendCallback), socket);
+		}
+
+		private void SendCallback(IAsyncResult ar)
+		{
+			try
+			{
+				// Retrieve the socket from the state object.
+				Socket socket = (Socket)ar.AsyncState;
+
+				// Complete sending the data to the remote device.
+				int bytesSent = socket.EndSend(ar);
+				Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
+		#endregion
+
+		protected bool Disconnect(Socket socket)
+		{
+			try
+			{
+				socket.Shutdown(SocketShutdown.Both);
+				socket.Close();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
